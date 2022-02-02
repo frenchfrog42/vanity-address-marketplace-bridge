a31b8b478bd80d01dad3548b54972604d9f6486f
frenchfrog42 <76942432+frenchfrog42@users.noreply.github.com>
2022-02-02 09:05:37 +0100

    Random change to test

--- transformer/process.js
+++ transformer/process.js
@@ -10,13 +10,16 @@ module.exports = async (state, action) => {
     }
 
     // Transactions where the key from s3 did not sign an input are invalid
+    /*
     if (!(action.in.some(input => input.e.a === action.out[0].s3))) {
       throw new Error(
         'HWP sender did not sign at least one input to the transaction!'
       )
     }
+    */
 
     // Message must exist, and not be an "f" attribute longer than 512 bytes
+    /*
     if (
       !action.out[0].s4 ||
         action.out[0].f4 ||
@@ -24,6 +27,7 @@ module.exports = async (state, action) => {
     ) {
       throw new Error('Message is either missing or longer than 512 bytes!')
     }
+    */
 
     // HWP message is constructed from the fields of the Bitcoin transaction
     const data = {
